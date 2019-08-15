da3065aa87cc742ba7ae6186669d3de2f31b9170
DSTHEDRAGON <drakendev@yahoo.com>
2019-08-15 20:23:22 +0100

    First Commit

---
+++ hash.js
@@ -0,0 +1,32 @@
+const bcrypt = require('bcrypt');
+const {MD5} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+// bcrypt.genSalt(10, (err, salt) => {
+//     if(err) return next(err)
+//    
+//     bcrypt.hash('password', salt, (err,hash) => {
+//         if(err) return next(err)
+//         console.log(hash)
+//     })
+// });
+
+// const secret = 'mysecretpassword';
+
+
+// const user = {
+//     id:1,
+//     token: MD5('SDFSFSDFSDF').toString()
+// }
+
+// console.log(user)
+
+const id = '1000';
+const secret = 'supersecret'
+
+const receivedToken = 'eyJhbGciOiJIUzI1NiJ9.MTAwMA.L9PmEqLlZjettygguzj25agunJu6NkvVtG9RFRBnK2Y'
+
+// const token = jwt.sign(id, secret);
+const decodeToken = jwt.verify(receivedToken, secret);
+
+console.log(decodeToken)
